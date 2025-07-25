583f8f5de3f0a56f8dbd32fbc0f235e317a303ac
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-24 21:16:04 -0300

    Centralizando temas e cores

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -1,3 +1,4 @@
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
@@ -10,11 +11,7 @@ class Header extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: <Color>[
-            Color.fromRGBO(103, 99, 234, 1.0),
-            Color.fromRGBO(155, 105, 254, 1.0),
-            Color.fromRGBO(195, 107, 255, 1.0)
-          ],
+          colors: ThemeColors.headerGradient,
         ),
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(10))
       ),
@@ -30,19 +27,15 @@ class Header extends StatelessWidget {
                 Text.rich(
                   TextSpan(
                     text: '\$',
-                    style: TextStyle(fontSize: 16),
                     children: <TextSpan>[
                       TextSpan(
                         text: '1000.00',
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                        ),
+                        style: Theme.of(context).textTheme.bodyLarge,
                       ),
                     ],
                   ),
                 ),
-                Text('Balanço disponível', style: TextStyle(fontSize: 16)),
+                Text('Balanço disponível'),
               ],
             ),
             Icon(Icons.account_circle, size: 42),
---
+++ lib/themes/my_theme.dart
@@ -0,0 +1,14 @@
+import 'package:alubank/themes/theme_colors.dart';
+import 'package:flutter/material.dart';
+
+ThemeData myTheme = ThemeData(
+  primarySwatch: ThemeColors.primaryColor,
+  primaryColor: ThemeColors.primaryColor,
+  brightness: Brightness.dark,
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      fontSize: 16
+    ),
+    bodyLarge: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)
+  )
+);
